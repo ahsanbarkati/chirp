5bab98bbefab296c5c8ebafa2fcbe06f53b76463
Abhimanyu Singh Gaur <12651351+abhimanyusinghgaur@users.noreply.github.com>
2020-08-05 21:13:23 +0530

    push

--- src/pages/searcher.js
+++ src/pages/searcher.js
@@ -10,7 +10,7 @@ import {CenteredCard} from "../components/card";
 
 export function Searcher() {
 
-  const queryTweet = gql`
+    const queryTweet = gql`
   query searchTweets($text:String, $first: Int!, $offset: Int!) {
     queryTweet(filter: {text: {anyoftext: $text}}, first: $first, offset: $offset) {
       text
@@ -24,76 +24,65 @@ export function Searcher() {
   }
     `;
 
-  var done = false;
+    var done = false;
 
-  const [anchorEl, setAnchorEl] = React.useState(null);
-  const open = Boolean(anchorEl);
-  const [searchText, setSearchText] = useState("");
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const open = Boolean(anchorEl);
+    const [searchText, setSearchText] = useState("");
 
-  const text = searchText;
-  const first = 10;
-  const offset = 0;
-  const {loading, error, data} = useQuery(queryTweet, {
-    variables: {text, first, offset}
-  });
+    const text = searchText;
+    const first = 10;
+    const offset = 0;
+    const {loading, error, data} = useQuery(queryTweet, {
+        variables: {text, first, offset}
+    });
 
-  if(!loading){
-    console.log("data", data);
-  }
+    if (!loading) {
+        console.log("data", data);
+    }
 
-  const handleClick = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
-  const handleClose = () => {
-    console.log(anchorEl);
-    console.log(searchText);
-    setAnchorEl(null);
-  };
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+    const handleClose = () => {
+        console.log(anchorEl);
+        console.log(searchText);
+        setAnchorEl(null);
+    };
 
-  const handlePeople = () => {
-    console.log("poeple");
-    console.log(searchText);
-    setAnchorEl(null);
-  };
+    const handlePeople = () => {
+        console.log("poeple");
+        console.log(searchText);
+        setAnchorEl(null);
+    };
 
-  const handleTweets = () => {
-    console.log("tweets");
-    console.log(searchText);
-    done = true;
-    setAnchorEl(null);
-  };
-  
+    const handleTweets = () => {
+        console.log("tweets");
+        console.log(searchText);
+        done = true;
+        setAnchorEl(null);
+    };
 
-  return (
-    <Grid>
-      <TextField id="Search People" onChange={e => setSearchText(e.target.value)} variant={"outlined"} multiline={true}></TextField>
-      <div>
-      {/* <Button aria-controls="fade-menu" aria-haspopup="true" onClick={handleClick}>
-        Search Options
-      </Button> */}
-      <Menu
-        id="fade-menu"
-        anchorEl={anchorEl}
-        keepMounted
-        open={open}
-        onClose={handleClose}
-        TransitionComponent={Fade}
-      >
-        <MenuItem onClick={handlePeople}>People</MenuItem>
-        <MenuItem onClick={handleTweets}>Tweets</MenuItem>
-      </Menu>
-    </div>
-    {!loading? <div>
-        {
-            data?.queryTweet.map(tweet =>
-            <Grid item xs={12} key={tweet?.text}>
-            <CenteredCard tweet={tweet}></CenteredCard>
-            </Grid>)
-            }
-        
-      </div> : <span></span>}
-    </Grid>
-  );
+    var tweets = data?.queryTweet
+    var hasError = false
+    if (tweets == null || tweets.length == 0 || loading) {
+        hasError = true
+    }
 
-  
+
+    return (
+        <Grid
+            container
+            direction="column"
+            justify="flex-start"
+            alignItems="center"
+            style={{padding: "0 20%"}}
+        >
+            <TextField id="Search People" style={{width: "100%"}} placeholder={"search here"} onChange={e => setSearchText(e.target.value)}
+                       variant={"outlined"} multiline={true}></TextField>
+            {!hasError ? data?.queryTweet.map(tweet => <CenteredCard tweet={tweet}></CenteredCard>) : <span>Nothing to display</span>}
+        </Grid>
+    );
+
+
 }
